450ef3e8a2321a7434965c97498dc63c9142db79
mathias <mathiasgheno@gmail.com>
2023-08-21 22:49:33 -0300

    chore: no frm drag and drop sort

--- src/Article/Article.jsx
+++ src/Article/Article.jsx
@@ -2,8 +2,24 @@ import React, {useState} from 'react';
 import './Article.css';
 import {Tag, Tags} from '../Tag/Tag.jsx';
 
-const ArticleCard = ({ title, tags, readTime, tabIndex }) => (
-  <div className="card" tabIndex={tabIndex}>
+const ArticleCard = ({
+   title,
+   tags,
+   readTime,
+   tabIndex,
+   onDragStart,
+   onDragOver,
+   onDragEnd,
+   isDragging,
+}) => (
+  <div
+    className={isDragging ? 'card dragging' : 'card'}
+    draggable
+    onDragStart={onDragStart}
+    onDragOver={onDragOver}
+    onDragEnd={onDragEnd}
+    tabIndex={tabIndex}
+  >
     <h2 className="title">{title}</h2>
     <div className="tags">
       {tags.map((tag, index) => (
@@ -15,6 +31,7 @@ const ArticleCard = ({ title, tags, readTime, tabIndex }) => (
       <span>{readTime}min</span>
     </div>
     <div className="actions">
+      {/*<button><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M278.6 9.4c-12.5-12.5-32.8-12.5-45.3 0l-64 64c-9.2 9.2-11.9 22.9-6.9 34.9s16.6 19.8 29.6 19.8h32v96H128V192c0-12.9-7.8-24.6-19.8-29.6s-25.7-2.2-34.9 6.9l-64 64c-12.5 12.5-12.5 32.8 0 45.3l64 64c9.2 9.2 22.9 11.9 34.9 6.9s19.8-16.6 19.8-29.6V288h96v96H192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l64 64c12.5 12.5 32.8 12.5 45.3 0l64-64c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8H288V288h96v32c0 12.9 7.8 24.6 19.8 29.6s25.7 2.2 34.9-6.9l64-64c12.5-12.5 12.5-32.8 0-45.3l-64-64c-9.2-9.2-22.9-11.9-34.9-6.9s-19.8 16.6-19.8 29.6v32H288V128h32c12.9 0 24.6-7.8 29.6-19.8s2.2-25.7-6.9-34.9l-64-64z"/></svg></button>*/}
       <button>
         <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M352 0c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9L370.7 96 201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L416 141.3l41.4 41.4c9.2 9.2 22.9 11.9 34.9 6.9s19.8-16.6 19.8-29.6V32c0-17.7-14.3-32-32-32H352zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z"/></svg>
       </button>
@@ -28,8 +45,83 @@ const ArticleCard = ({ title, tags, readTime, tabIndex }) => (
   </div>
 );
 
-export const Articles = ({articles, changeActiveInTag, tags, selected}) => {
+let canvas;
+
+export const Articles = ({ articles, changeActiveInTag, tags, selected }) => {
   const [showTags, setShowTags] = useState(false);
+  const [draggedIndex, setDraggedIndex] = useState(null);
+  const [draggedOverIndex, setDraggedOverIndex] = useState(null);
+  const [reorderedArticles, setReorderedArticles] = useState(articles);
+
+  const handleDragStart = (e, index) => {
+    console.log('handleDragStart');
+    setDraggedIndex(index);
+
+    let clonedDraggable = e.target.cloneNode(true);
+
+    clonedDraggable.style.display = 'block';
+    clonedDraggable.style.opacity = '0.5';
+    clonedDraggable.style.position = 'absolute';
+    clonedDraggable.style.top = '-1000px';
+    clonedDraggable.style.left = '-1000px';
+    clonedDraggable.style.borderRadius = '0px';
+
+    document.body.appendChild(clonedDraggable);
+
+    canvas = document.createElement('canvas');
+    canvas.width = clonedDraggable.offsetWidth;
+    canvas.height = clonedDraggable.offsetHeight;
+
+    // Tornar o canvas invisível
+    canvas.style.visibility = 'hidden';
+    document.body.appendChild(canvas);
+
+    let ctx = canvas.getContext('2d');
+
+    // Definir a cor de fundo do canvas para a mesma cor de fundo da página
+    ctx.fillStyle = getComputedStyle(document.body).backgroundColor;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = getComputedStyle(clonedDraggable).backgroundColor;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.font = '20px Arial';
+    ctx.fillStyle = getComputedStyle(clonedDraggable).color;
+    ctx.fillText(clonedDraggable.textContent.trim(), 10, 30);
+
+    let img = new Image();
+    img.onload = function() {
+      e.dataTransfer.setDragImage(img, 50, 50);
+    }
+    img.src = canvas.toDataURL();
+
+    document.body.removeChild(clonedDraggable);
+    console.log('handleDragStart FIM');
+  };
+
+  const handleDragOver = (index) => {
+    console.log('handleDragOver');
+    if (index !== draggedIndex) {
+      setDraggedOverIndex(index);
+    }
+  };
+
+  const handleDragEnd = () => {
+    console.log('handleDragEnd');
+
+    if (canvas) {
+      document.body.removeChild(canvas);
+    }
+
+    if (draggedOverIndex !== null) {
+      const updatedArticles = [...reorderedArticles];
+      const draggedArticle = updatedArticles.splice(draggedIndex, 1)[0];
+      updatedArticles.splice(draggedOverIndex, 0, draggedArticle);
+
+      setReorderedArticles(updatedArticles);
+      setDraggedOverIndex(null);
+    }
+    setDraggedIndex(null);
+  };
 
   return (
     <div className="articles">
@@ -37,22 +129,25 @@ export const Articles = ({articles, changeActiveInTag, tags, selected}) => {
         <h3>Articles</h3>
         <button onClick={() => setShowTags(state => !state)}><svg className="icon" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M3.9 54.9C10.5 40.9 24.5 32 40 32H472c15.5 0 29.5 8.9 36.1 22.9s4.6 30.5-5.2 42.5L320 320.9V448c0 12.1-6.8 23.2-17.7 28.6s-23.8 4.3-33.5-3l-64-48c-8.1-6-12.8-15.5-12.8-25.6V320.9L9 97.3C-.7 85.4-2.8 68.8 3.9 54.9z"/></svg></button>
       </div>
-      {
-        showTags && (
-          <Tags
-            tags={tags}
-            onClick={changeActiveInTag}
-            selected={selected}
-          />
-        )
-      }
+      {showTags && <Tags tags={tags} onClick={changeActiveInTag} selected={selected} />}
       <div className="list">
-        {
-          articles.map(article => <ArticleCard {...article} tabIndex={0} />)
-        }
+        {reorderedArticles.map((article, index) => (
+          <div
+            key={article.id}
+          >
+            <ArticleCard
+              {...article}
+              tabIndex={0}
+              onDragStart={(e) => handleDragStart(e, index)}
+              onDragOver={() => handleDragOver(index)}
+              onDragEnd={handleDragEnd}
+              isDragging={draggedIndex === index}
+            />
+          </div>
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default ArticleCard;
--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 export const Header = () => (
   <header className="header container">
     <button><svg className="icon" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg></button>
-    Minimal Article
+    Minimal Article Repository
     <button><svg className="icon" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"/></svg></button>
   </header>
 )
